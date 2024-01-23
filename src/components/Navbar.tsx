1343d9ea708e5fa640fa972ad3a38742b7118771
Aldo Wanderley Costa Junior <aldowanderley11@gmail.com>
2024-01-22 23:25:06 -0300

    :sparkles: feat: adding dropdown and storybook

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link"
-import { useSearchParams } from "next/navigation"
+import { usePathname, useSearchParams } from "next/navigation"
+import { DropDown } from "./Dropdown"
 
 const activeLinkClass = "text-base font-semibold text-[#41414D] cursor-pointer border-b-4 border-b-[#FFA585]"
 const inactiveLinkClass = "text-base font-normal text-[#737380] cursor-pointe"
@@ -7,15 +8,21 @@ const inactiveLinkClass = "text-base font-normal text-[#737380] cursor-pointe"
 export const Navbar = () => {
   const searchParams = useSearchParams()
   const params = new URLSearchParams(searchParams)
-  const productActive = params.get("product")
+  const pathName = usePathname();
 
+  const productActive = params.get("product")
+  
   return (
-    <nav className="flex items-center	gap-x-10">
-      <Link href="/"
-        className={!productActive ? activeLinkClass : inactiveLinkClass}
-      >Todos os produtos</Link>
-      <Link href="/?product=camisetas" className={productActive === 'camisetas' ? activeLinkClass : inactiveLinkClass}>Camisetas</Link>
-      <Link href="/?product=canecas" className={productActive === 'canecas' ? activeLinkClass: inactiveLinkClass}>Canecas</Link>
+    <nav className="flex items-center justify-between">
+      <div className="flex items-center	gap-x-10">
+        <Link href={`${pathName}`}
+          className={!productActive ? activeLinkClass : inactiveLinkClass}
+        >Todos os produtos</Link>
+        <Link href={`${pathName}?product=camisetas`} className={productActive === 'camisetas' ? activeLinkClass : inactiveLinkClass}>Camisetas</Link>
+        <Link href={`${pathName}?product=canecas`} className={productActive === 'canecas' ? activeLinkClass: inactiveLinkClass}>Canecas</Link>
+      </div>
+
+      <DropDown />
     </nav>  
   )
 }
